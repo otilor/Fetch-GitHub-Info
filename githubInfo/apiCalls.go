843a2f5f44b5e88844ad6259b11f8155f499cd12
Gabriel Akinyosoye <gabrielfemi799>
2020-06-09 10:30:10 -0400

    Achievement: call the GitHub API v3 via go-github library and get a specific user's organizations
    Took 11 minutes

--- githubInfo/apiCalls.go
+++ githubInfo/apiCalls.go
@@ -1,6 +1,18 @@
 package githubInfo
 
+import (
+	"context"
+	"github.com/google/go-github/github"
+	"github.com/sirupsen/logrus"
+)
 
 func getDetails(username string) {
-
+	client := github.NewClient(nil)
+	ctx := context.Background()
+	//list all organizations for user GabielFemi
+	orgs, _, err := client.Organizations.List(ctx, username, nil)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+	logrus.Println(username, " has joined ", len(orgs), " GitHub organizations ")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/GabielFemi/Fetch-GitHub-Info
 go 1.14
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/sirupsen/logrus v1.6.0
 )
