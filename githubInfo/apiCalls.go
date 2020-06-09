fc08ebca90e1eaddd626502423bc12a7ceb8df0d
Gabriel Akinyosoye <gabrielfemi799>
2020-06-09 10:38:55 -0400

    Append user details to UserInfo data type
    Took 3 minutes

--- githubInfo/apiCalls.go
+++ githubInfo/apiCalls.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func getDetails(username string) {
+func getDetails(username string) UserInfo{
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	//list all organizations for user GabielFemi
@@ -14,5 +14,9 @@ func getDetails(username string) {
 	if err != nil {
 		logrus.Fatalln(err)
 	}
+	var details UserInfo
+	details.Username = username
+	details.NumberOfJoinedOrganizations = len(orgs)
 	logrus.Println(username, " has joined ", len(orgs), " GitHub organizations ")
+	return details
 }
