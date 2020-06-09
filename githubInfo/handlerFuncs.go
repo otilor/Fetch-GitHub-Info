e36d6b09e8db0444770e67f5150a312de52f940d
Gabriel Akinyosoye <gabrielfemi799>
2020-06-09 08:09:36 -0400

    Render HTML page when / route is hit

--- githubInfo/handlerFuncs.go
+++ githubInfo/handlerFuncs.go
@@ -3,7 +3,7 @@ package githubInfo
 import "net/http"
 
 func Index(w http.ResponseWriter, r *http.Request) {
-
+	render(w, "index.html", r)
 }
 
 func Search(w http.ResponseWriter, r *http.Request) {
